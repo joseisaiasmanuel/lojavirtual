ea606fa35d4cfc56cdd75b54abf10225b733b171
jose1704 <joseisaiasmanuel@gmail.com>
2021-03-04 12:48:04 +0100

    recuperacaoSenha

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -71,7 +71,27 @@ class _LoginScreenState extends State<LoginScreen> {
                   Align(
                     alignment: Alignment.centerRight,
                     child: FlatButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        if(_emailController.text.isEmpty)
+                          _scaffoldKey.currentState.showSnackBar(
+                              SnackBar(content: Text("Insira seu e-mail para recuperar !"),
+                                backgroundColor: Colors.redAccent,
+                                duration: Duration(seconds: 2),
+                              )
+                          );
+                        else {
+                          model.recoverPass(_emailController.text);
+                          _scaffoldKey.currentState.showSnackBar(
+                              SnackBar(content: Text("confira seu email !"),
+                                backgroundColor: Theme
+                                    .of(context)
+                                    .primaryColor,
+                                duration: Duration(seconds: 2),
+                              )
+                          );
+                        }
+
+                      },
                       child: Text("Esqueci minha senha",
                           textAlign: TextAlign.right),
                       padding: EdgeInsets.zero,
