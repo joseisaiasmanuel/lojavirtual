010c55e6bf0a30e963644eb0b6dc6e372e7f6802
jose1704 <joseisaiasmanuel@gmail.com>
2021-03-04 09:37:02 +0100

    cadastroC

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,67 +1,110 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:scoped_model/scoped_model.dart';
-
 
 class UserModel extends Model {
   FirebaseAuth _auth = FirebaseAuth.instance;
   FirebaseUser firebaseUser;
-  Map<String, dynamic> userData= Map();
-  bool isLoading= false ;
- // login
+  Map<String, dynamic> userData = Map();
 
-  void signUp({@required Map<String, dynamic> userData, @required String pass,@required VoidCallback onSuccess, @required VoidCallback onFail}) {
-    isLoading =true;
+  //usuario actual
+  bool isLoading = false;
+
+  static UserModel of(BuildContext context) =>
+      ScopedModel.of<UserModel>(context);
+
+  @override
+  void addListener(VoidCallback listener) {
+    super.addListener(listener);
+    _loadCurrentUser();
+  } //Crian conta
+
+  void signUp({
+    @required Map<String, dynamic> userData,
+    @required String pass,
+    @required VoidCallback onSuccess,
+    @required VoidCallback onFail,
+  }) {
+    isLoading = true;
     notifyListeners();
-    _auth.createUserWithEmailAndPassword(email: userData["email"], password: pass).then(
-            (user)  async{
-              firebaseUser= user;
-              await _saveUserData(userData);
-              onSuccess();
-              isLoading= false;
-              notifyListeners();
-            }).catchError((e){
-            onFail();
-            isLoading= false;
-            notifyListeners();
+    _auth
+        .createUserWithEmailAndPassword(
+        email: userData["email"], password: pass)
+        .then((user) async {
+      firebaseUser = user;
+
+      await _saveUserData(userData);
+      onSuccess();
+
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
     });
-
-
-
   }
 
-
-  // cadatro
-void signIn() async{
-    isLoading=true;
+  void signIn({
+    @required String email,
+    @required String pass,
+    @required VoidCallback onSuccess,
+    @required VoidCallback onFail,
+  }) async {
+    isLoading = true;
     notifyListeners();
 
-    await Future.delayed(Duration(seconds: 3));
+    _auth
+        .signInWithEmailAndPassword(email: email, password: pass)
+        .then((user) async {
+      firebaseUser = user;
+      await _loadCurrentUser();
+      onSuccess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
+  }
 
-    isLoading= false;
+  void recoverPass(String email) {
+    _auth.sendPasswordResetEmail(email: email);
+  }
+
+  bool isLoggedIn() {
+    return firebaseUser != null;
+  }
+
+  void signOut() async {
+    await _auth.signOut();
+    userData = Map();
+    firebaseUser = null;
     notifyListeners();
+  }
 
-}
+  Future<Null> _saveUserData(Map<String, dynamic> userData) async {
+    this.userData = userData;
+    await Firestore.instance
+        .collection("users")
+        .document(firebaseUser.uid)
+        .setData(userData);
+  }
 
-
-  // recuperar senha
-void recoverPass(){
-
-}
-
-Future<Null> _saveUserData(Map<String,dynamic> userData) async{
-    this.userData= userData;
-    await Firestore.instance.collection("users").document(firebaseUser.uid).setData(userData);
-}
-
-  // usuarioa logado
-void isLoginIn(){
-
-}
-
-
-
-
+  Future<Null> _loadCurrentUser() async {
+    if (firebaseUser == null) firebaseUser = await _auth.currentUser();
+    if (firebaseUser != null) {
+      if (userData["name"] == null) {
+        DocumentSnapshot docUser = await Firestore.instance
+            .collection("users")
+            .document(firebaseUser.uid)
+            .get();
+        userData = docUser.data;
+      }
+    }
+    notifyListeners();
+  }
 }
--- lib/screen/signup_screen.dart
+++ lib/screen/signup_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 
@@ -8,114 +10,120 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-  final _nameController= TextEditingController();
-  final _emailController= TextEditingController();
-  final _passController= TextEditingController();
-  final _addressController= TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
-  final  _formKey=GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+
+  final _emailController = TextEditingController();
+
+  final _passController = TextEditingController();
+
+  final _addressController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Criar conta",
-          ),
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text(
+          "Criar conta",
         ),
-        body: ScopedModelDescendant<UserModel>(
-          builder: (context,child,model){
-            if(model.isLoading)
-              return Center(child: CircularProgressIndicator(),);
-            return  Form(
+        centerTitle: true,
+      ),
+      body: ScopedModelDescendant<UserModel>(
+        builder: (BuildContext context, Widget child, UserModel model) {
+          if (model.isLoading)
+            return Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.blue,
+                ));
+          return Center(
+            child: Form(
               key: _formKey,
-              child: ListView(
-                padding: EdgeInsets.all(16.0),
-                children: <Widget>[
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                        hintText: "Nome Completo"),
-                    validator: (text){
-                      if(text.isEmpty) return "Preencha o nome";
-                    },
-                  ),
-                  SizedBox(height: 16.0,),
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                        hintText: "E-mail"),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (text){
-                      if(text.isEmpty || !text.contains("@")) return "E-mail Inválido";
-                    },
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  TextFormField(
-                    controller: _passController,
-                    decoration: InputDecoration(hintText: "Senha"),
-                    obscureText: true,
-                    validator: (text){
-                      if(text.isEmpty || text.length<6) return"A senha deve conter 6 caracter";
-                    },
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  TextFormField(
-                    controller: _addressController,
-                    decoration: InputDecoration(
-                        hintText: "Endereço"),
-                    validator: (text){
-                      if(text.isEmpty ) return "Preencha o endereço";
-                    },
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  SizedBox(
-                    height: 44.0,
-                    child: RaisedButton(
-                      child: Text(
-                        "Criar Conta",
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                      textColor: Colors.white,
-                      color: Theme.of(context).primaryColor,
-                      onPressed: () {
-                        if(_formKey.currentState.validate()){
-                          Map<String ,dynamic> userData={
-                            "name":_nameController,
-                            "email":_emailController,
-                            "address":_addressController
-
-                          };
-                          model.signUp(
-                              userData: userData,
-                              pass: _passController.text,
-                              onSuccess:
-                              _onSucess,
-                              onFail: _onFail
-                          );
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(hintText: "Nome Completo"),
+                      validator: (text) {
+                        if (text.isEmpty) return "Digite o nome completo";
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(hintText: "E-mail"),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (text) {
+                        if (text.isEmpty || !text.contains("@")) {
+                          return "E-mail Invalido";
                         }
                       },
                     ),
-                  )
-                ],
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _passController,
+                      decoration: InputDecoration(hintText: "Senha"),
+                      obscureText: true,
+                      validator: (text) {
+                        if (text.isEmpty || text.length < 6) {
+                          return "Senha invalida";
+                        }
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _addressController,
+                      decoration: InputDecoration(hintText: "Endereço"),
+                      validator: (text) {
+                        if (text.isEmpty) return "Endereço invalido";
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Container(
+                        height: 44,
+                        child: RaisedButton(
+                          child: Text("Cadastrar"),
+                          textColor: Colors.white,
+                          color: Theme.of(context).primaryColor,
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              Map<String, dynamic> userData = {
+                                "name": _nameController.text,
+                                "email": _emailController.text,
+                                "address": _addressController.text,
+                              };
+                              model.signUp(
+                                userData: userData,
+                                pass: _passController.text,
+                                onSuccess: _onSuccess,
+                                onFail: _onFail,
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            );
-          },
-        )
+            ),
+          );
+        },
+      ),
     );
   }
-  void _onSucess(){
+
+  void _onSuccess() {
 
   }
 
-  void _onFail(){
+  void _onFail() {
 
   }
 }
-
-
