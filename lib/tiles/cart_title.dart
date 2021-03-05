b69022606a9dac8e07849f46dc606a09d93c010e
jose1704 <joseisaiasmanuel@gmail.com>
2021-03-05 01:04:26 +0100

    logicaCarrinho

---
+++ lib/tiles/cart_title.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/data/cart_product.dart';
+class CartTitle extends StatelessWidget {
+ final CartProduct cartProduct;
+
+  CartTitle(this.cartProduct);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
