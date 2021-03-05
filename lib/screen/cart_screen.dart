e9cec9b25fed988693d00ae705a3dac09ac7f4f2
jose1704 <joseisaiasmanuel@gmail.com>
2021-03-05 13:49:09 +0100

    recuperacaoCarrinho

--- lib/screen/cart_screen.dart
+++ lib/screen/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screen/login_screen.dart';
+import 'package:loja_virtual/tiles/cart_title.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -64,6 +65,16 @@ class CartScreen extends StatelessWidget {
               child: Text("Nenhum produto no carrinho", style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),
                textAlign: TextAlign.center,
               ),
+            );
+          }else {
+            return ListView(
+              children:<Widget> [
+                Column(
+                  children: model.products.map((product){
+                    return CartTitle(product);
+                  }).toList()
+                )
+              ],
             );
           }
         },
